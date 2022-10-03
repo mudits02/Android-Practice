153076be1b02cd71d8d1e52a7ed159626f6c3c1f
unknown <mudits02@gmail.com>
2022-10-03 09:14:05 +0530

    kotlin_basics

---
+++ Kotlinlang_basics/app/src/main/java/in/mudits02/kotlinlang_basics/kotlinlang_basics_1.kt
@@ -0,0 +1,24 @@
+package `in`.mudits02.kotlinlang_basics
+
+fun main()
+{
+    var myName = "Mudit"
+    print("Hello " + myName)
+
+    // var name : datatype = value <- this is the syntax for variable
+    var number = 31
+    var myNum : Int = 56
+    var myFloat : Float = 31.46
+    var myDouble : Double = 3.14287658972
+    val sentence = "Hello Mudit Shukla"
+
+    var sentence_length = sentence.length
+    var char_from_sentence = sentence [sentence_length - 1]
+
+
+    // String Interpolation Concept
+
+
+    //print(number)
+    //print(myNum)
+}
