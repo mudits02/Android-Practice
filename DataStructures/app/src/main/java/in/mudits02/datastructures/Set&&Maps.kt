bd1733efbbb75389bed8c9cc3b8c89d8a1c9a567
unknown <mudits02@gmail.com>
2023-01-14 17:36:56 +0530

    DS

--- DataStructures/app/src/main/java/in/mudits02/datastructures/Set&&Maps.kt
+++ DataStructures/app/src/main/java/in/mudits02/datastructures/Set&&Maps.kt
@@ -7,7 +7,36 @@ fun main()
     that property is shown in the following syntax
      */
     val fruit = setOf("Apple" , "Orange" , "Loda" , "Apple")
-    println(fruit.size)
+    //println(fruit.size)
+    //println(fruit.toSortedSet())
+
+
+    val newFruit = fruit.toMutableList()
+    newFruit.add("Pear")
+    newFruit.add("Water Melon")
+    println(newFruit)
+    println(newFruit.elementAt(index = 2))
+
+
+    //HashMaps
+    //Same as Ordered Map in C++
+    val weekdays = mapOf(1 to "Mon" , 2 to "Tue" , 3 to "Wed" , 4 to "Thur")
+    for (key in weekdays.keys)
+    {
+        println("$key is for the day of ${weekdays[key]}")
+    }
+
+    //Mutable maps
+    val newWeekday = weekdays.toMutableMap()
+    newWeekday[5] = "Fri"
+    newWeekday[6] = "Sat"
+    newWeekday[7] = "Sun"
+
+    for(key in newWeekday.keys)
+    {
+        println("$key is the number of the day of ${newWeekday[key]}")
+
+    }
 
 
 
