018149284988e508a868856b5250523840ea3dde
unknown <mudits02@gmail.com>
2023-01-14 23:36:42 +0530

    Data Structure finished

---
+++ DataStructures/app/src/main/java/in/mudits02/datastructures/Lambda Fucntion.kt
@@ -0,0 +1,32 @@
+package `in`.mudits02.datastructures
+
+
+/*
+    A lambda function is function with no name
+
+    they are function literals i.e. functions that are not declared but passed as
+    an expression
+
+    Open keyword -> in Kotlin we need to manually make the class inheritable by using keyword called open
+ */
+
+fun main()
+{
+    // addition of two numbers using lambda function
+    val sum : (Int , Int) -> Int = {a : Int , b : Int -> a + b}
+    println(sum(10 , 5))
+    // even shorter
+
+    val shorter_sum = {a : Int , b : Int -> println(a + b)}
+    println(shorter_sum(10 , 10))
+
+}
+
+/*
+    4 types of visibilty modifiers
+    1. Public
+    2. Private
+    3. Protected
+    4. Internal
+    Now internal is something new which makes the use of all the variables and functions inside the internal class onyl
+ */
