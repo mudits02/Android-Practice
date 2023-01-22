a5f181a7cbc8a09bc2c01bfe64bb0c62891b4a4b
unknown <mudits02@gmail.com>
2023-01-22 23:15:23 +0530

    App - 02 started

---
+++ Calculator/app/src/main/java/in/mudits02/calculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package `in`.mudits02.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
